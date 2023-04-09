964da0ca44f429427c51473d0c9ccebd986a3bdb
Ouahib <ouahib>
2023-04-09 09:27:07 +0000

    fist

---
+++ src/main/java/com/wahib/employeemanager/EmployeeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.wahib.employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerApplication.class, args);
+	}
+
+}
